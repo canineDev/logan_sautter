7b5073627bbea21a788705b12a15d570238c0554
canineDev <73805587+canineDev@users.noreply.github.com>
2025-04-22 19:42:17 +0000

    Tested in codespace

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,17 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def hellow_world():
+	return "Hello world"
+
+@app.route("/api/data")
+def get_data():
+	return jsonify({
+		"message": "Flask data",
+		"status": "success"
+	})
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=5000)
